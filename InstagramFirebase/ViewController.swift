326c0d5074808d9e7891163e202610135b33c18f
Your Name <yamadou21@gmail.com>
2019-03-01 10:42:04 +0000

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Yamadou Traore on 3/1/19.
+//  Copyright © 2019 yamadou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
